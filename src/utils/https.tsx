a87e070880532b398001c04d3f357ddc6ceb79b5
Igor Balos <ibalosh@gmail.com>
2025-06-04 11:49:35 +0200

    add auth token

--- src/utils/https.tsx
+++ src/utils/https.tsx
@@ -15,12 +15,14 @@ type RecipeProps = {
 }
 
 const apiUrl = import.meta.env.VITE_API_BASE_URL;
+const authHeaders = { "X-API-Token": import.meta.env.VITE_API_TOKEN }
 
 export async function fetchRecipes({ searchTerm, page, signal }: RecipesProps) {
     let url = `${apiUrl}/recipes`;
     url += '?search=' + searchTerm;
 
-    const response = await fetch(`${url}&page=${page}`, { signal: signal });
+    const response = await fetch(`${url}&page=${page}`,
+        { signal: signal , headers: authHeaders });
 
     if (!response.ok) {
         const error = new Error("An error occurred while fetching the events");
@@ -35,7 +37,7 @@ export async function fetchRecipe({id, signal }: RecipeProps): Promise<RecipeDet
     const url = `${apiUrl}/recipes/${id}`;
     console.log(url)
 
-    const response = await fetch(url, { signal: signal });
+    const response = await fetch(url, { signal: signal, headers: authHeaders});
     if (!response.ok) {
         throw new Error("Failed to fetch recipe");
     }
