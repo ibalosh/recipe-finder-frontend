7cc3eaadeee914b31c25d2ae80d5f0f360b78658
Igor Balos <ibalosh@gmail.com>
2025-06-04 16:51:57 +0200

    code cleanup

--- src/components/RecipeItem.tsx
+++ src/components/RecipeItem.tsx
@@ -12,12 +12,18 @@ export default function RecipeItem({recipe}: Props){
     const [loaded, setLoaded] = useState(false);
     const navigate = useNavigate();
 
-    function hey() {
+    function visitRecipePage() {
         navigate(`/recipes/${recipe.id}`)
     }
 
+    function formattedIngredients(ingredients: string[]) {
+        return ingredients.join(', ').
+            slice(0, 50) + (recipe.ingredients.join(', ').
+            length > 100 ? '...' : '')
+    }
+
     return (
-        <li className="recipe-item" onClick={hey}>
+        <li className="recipe-item" onClick={visitRecipePage}>
             <article>
                 <div className={`image-wrapper ${loaded ? 'loaded' : 'loading'}`}>
                     <img
@@ -26,17 +32,18 @@ export default function RecipeItem({recipe}: Props){
                         onLoad={() => setLoaded(true)}
                     />
                 </div>
-                <div>
-                    <div className="recipe-item-details">
-                        <h3 className="recipe-item-title">{recipe.title}</h3>
-                        <div className="recipe-meta">
-                            <span className="recipe-tag category">🍽 {recipe.category.name}</span>
-                            <span className="recipe-tag author">
-                                { recipe.author ? `👩‍🍳 ${recipe.author.name}` : `👤 Deleted user`}
-                            </span>
-                        </div>
-                        <div className="recipe-item-rating"><StarRating rating={recipe.ratings} /></div>
+
+                <div className="recipe-item-details">
+                    <h3 className="recipe-item-title">{recipe.title}</h3>
+                    <div className="recipe-meta">
+                        <div className="recipe-ingredients-text">{formattedIngredients(recipe.ingredients)}</div>
+                        <span className="recipe-tag category">🍽 {recipe.category.name}</span>
+                        <span className="recipe-tag author">
+                        { recipe.author ? `👩‍🍳 ${recipe.author.name}` : `👤 Deleted user`}
+                    </span>
                     </div>
+
+                    <div className="recipe-item-rating"><StarRating rating={recipe.ratings} /></div>
                 </div>
             </article>
         </li>
